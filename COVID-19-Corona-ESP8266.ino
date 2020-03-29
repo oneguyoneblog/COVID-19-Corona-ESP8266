7ccbc831a1f542e8456c39b6922503475066d030
One Guy, One Blog <OneGuy-OneBlog@users.noreply.github.com>
2020-03-29 03:45:39 +0200

    Update COVID-19-Corona-ESP8266.ino

--- COVID-19-Corona-ESP8266.ino
+++ COVID-19-Corona-ESP8266.ino
@@ -2,7 +2,7 @@
 
 	Get COVID-19 Corona REST API data with the ESP8266 microcontroller
 
-	Code example; for more informatio see:
+	Code example; for more information see:
 	https://oneguyoneblog.com/2020/03/28/esp8266-real-time-covoid-19-data-monitor/
 
 
